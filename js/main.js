af95f3b7cf87cde6e646024f6594f928290b2f28
perei@ampa <pereira13amparo@gmail,com>
2023-06-05 15:42:02 -0300

    accion 1 finalizada

--- js/main.js
+++ js/main.js
@@ -1,3 +1,50 @@
+class Prestamo{
+    constructor(monto,cantCuotas){
+        this.monto = monto;
+        this.interesApagar = 0;
+        this.cantCuotas = cantCuotas;
+        this.amortizacion = 0;
+        this.cuotasApagar = cantCuotas;
+        this.cuotasPagadas = 0;
+    }
+
+    getMonto(){
+        return this.monto;
+    }
+
+    setInteres(interes){
+        this.interesApagar = interes; 
+    }
+
+    getInteres(){
+        return this.interesApagar;
+    }
+
+    setAmortizacion(amortizacion){
+        this.amortizacion = amortizacion;
+    }
+
+    getAmortizacion(){
+        return this.amortizacion;
+    }
+
+    getCantCuotas(){
+        return this.cantCuotas;
+    }
+
+    getCuotasApagar(){ 
+        return this.cuotasApagar;
+    }
+
+    setCuotasPagadas(pagadas){
+        this.cuotasPagadas = pagadas;
+    }
+
+    getCuotasPagadas(){
+        return this.cuotasPagadas;
+    }
+}
+
 function MontoPrestamo(){
     let montoPrestamo = parseInt(prompt("Ingrese el monto a solicitar (por favor ingrese un valor entre 10.000 y 100.000)"));
     while(montoPrestamo > 100000 || montoPrestamo < 10000 || isNaN(montoPrestamo)){
@@ -20,30 +67,59 @@ function CantidadCuotas(){
     return cantidadCuotas;
 }
 
-function Calculos(monto, cuotas){
+function Calculos(prestamo){
+    let monto = prestamo.getMonto();
+    let cantCuotas = prestamo.getCantCuotas();
     let tasaMensual = 0.0272 * 1.22;
-    let cuota = monto * tasaMensual * Math.pow(1 + tasaMensual, cuotas) / (Math.pow(1 + tasaMensual, cuotas) - 1);
-    let interesTotal = cuota * cuotas - monto;
-    let amortizacionPrestamo = monto/cuotas;
-    alert("Monto del Prestamo: " + monto + " pesos\n" +
-    "Amortización Constante: "  + amortizacionPrestamo.toFixed(2) + " pesos\n" +
-    "Interés total a pagar: " + interesTotal.toFixed(2) + " pesos\n" +
-    "Cuota mensual: " + cuota.toFixed(2) + " pesos\n");
+    let cuota = (monto * tasaMensual * Math.pow(1 + tasaMensual, cantCuotas) / (Math.pow(1 + tasaMensual, cantCuotas) - 1)).toFixed(2);
+    let interesTotal = (cuota * cantCuotas - monto).toFixed(2);
+    let amortizacionPrestamo = (monto/cantCuotas).toFixed(2);
+    prestamo.setAmortizacion(amortizacionPrestamo);
+    prestamo.setInteres(interesTotal);
+    alert("Monto del Prestamo: " + prestamo.getMonto() + " pesos\n" +
+    "Amortización Constante: "  + amortizacionPrestamo + " pesos\n" +
+    "Interés total a pagar: " + interesTotal + " pesos\n" +
+    "Cuota mensual: " + cuota + " pesos\n");
 }
 
-function calcularPrestamo(){
-    let repetir = true
+function listarPrestamos(lista){
+    for (let i = 0; i< lista.length; i++) {
+        let p = lista[i];
+        alert("prestamo numero " + (i+1) + ": " +
+                 "\n Monto total: " + p.getMonto() + " pesos" + 
+                 "\n Cantidad de cuotas a pagar: " + p.getCantCuotas() + 
+                 "\n Amortizacion Constante: " + p.getAmortizacion() + " pesos" +
+                 "\n Interes total a pagar: " + p.getInteres() + " pesos" +
+                 "\n Cuotas a pagar: ");
+    }
+}
+
+function inicio(){
+    let repetir = true;
+    let prestamosCliente = [];
     while(repetir){
-        let Prestamo = MontoPrestamo();
-        let Cuotas = CantidadCuotas();
-        Calculos(Prestamo, Cuotas);
-        let respuesta = (prompt("Desea probar con otro monto? (Si o No)")).toUpperCase();
-        if(respuesta != "SI"){
+        let accion = prompt("ingrese el numero que corresponda a la accion que quiera hacer \n 1.ingresar un prestamo \n 2.Ver los prestamos ingresados \n 3.Pagar una Cuota \n 4.salir")
+        if(accion == 1){
+            let Monto = MontoPrestamo();
+            let Cuotas = CantidadCuotas();
+            let prestamo = new Prestamo(Monto,Cuotas);
+            Calculos(prestamo);
+            prestamosCliente.push(prestamo);
+        }
+        else if(accion == 2){
+            listarPrestamos(prestamosCliente);
+        }
+        else if(accion == 3){
+            alert("WIP, wippy, wippy good");
+        }
+        else if(accion == 4){
             repetir = false;
+        }
+        else{
+            alert("se ha ingresado una accion incorrecta, porfavor re-ingrese su seleccion.");
         }
     }
 }
 
 
-
-calcularPrestamo();
+inicio();
